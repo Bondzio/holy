ec92f3d73ed2aa96380bce3b90a19cdebfd8d3a0
reut <vutzster@gmail.com>
2014-01-21 17:55:19 +0200

    add contextual links render in template

--- sites/all/themes/subomega/templates/region--content.tpl.php
+++ sites/all/themes/subomega/templates/region--content.tpl.php
@@ -1,5 +1,7 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>  
+    <?php print render($title_prefix); ?>
+
 <?php  
   if (arg(0) == 'node' && is_numeric(arg(1)) ) {
   // Get the nid
@@ -16,8 +18,11 @@
   	 print '<h1 class="title" id="page-title">'.$title.'</h1>';
   }
 ?>
+ <?php print render($title_suffix); ?>
+
   <?php if ($tabs && !empty($tabs['#primary'])): ?><div class="tabs clearfix"><?php print render($tabs); ?></div><?php endif; ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+
 <?php
 print $content; 
 ?>
